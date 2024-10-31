6b32c828a7440e66df5ffc22e53d60b51a5845b5
Anthony Brown <75460744+tonyBCod1ng@users.noreply.github.com>
2024-10-31 15:41:36 -0400

    refactored code, updated typing library

--- src/Projects.js
+++ src/Projects.js
@@ -1,8 +1,16 @@
 import React from "react";
+import {Col, Row} from "react-bootstrap";
 
 
 export default function Projects(){
     return(
-        <div></div>
+        <Row className={"justify-content-center"} style={{marginTop:"25px", lineHeight:"40px"}}>
+            <Col className={"col-12"}>
+                <h3>Here is where I will list the projects I am working on</h3>
+            </Col>
+            <Col className={"col-4"}>
+                <h3>Stay tuned I'm cooking!</h3>
+            </Col>
+        </Row>
     )
 }
